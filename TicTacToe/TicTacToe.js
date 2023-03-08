0c057b46e99b018aa2ba78f80342fba12308c48b
Charlotte Sass <Jeffreymsass@gmail.com>
2023-03-08 17:43:11 -0500

    tictactoe javascript and some css

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -2,10 +2,29 @@
 let gamestate = {
   player: [{ name: "" }, { name: "" }],
 };
+
 //create gameboard
 let gameboard = document.getElementById("gameboard");
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < 9; i++) {
   let boardel = document.createElement("div");
   boardel.classList.add("board");
   gameboard.append(boardel);
 }
+//player name submit
+let input = document.getElementById("submit");
+function nameadd(input) {
+  if (gamestate.player[0].name == true) {
+    gamestate.player.name = input;
+  }
+  gamestate.player.name += input;
+}
+//list name
+
+//reset board game
+//check if tile is taken
+//check if tie
+//check for win
+//
+input.addEventListener("click", function () {
+  nameadd(document.querySelector("input").value);
+});
