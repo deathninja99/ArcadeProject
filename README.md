6354f8cc829466a9c78a32a1ba21aebe29ed9fd0
Charlotte Sass <Jeffreymsass@gmail.com>
2023-03-09 11:04:23 -0500

    got names to work

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-this is where i will put something
+#to-do
+-create images for index
+-style index
+-make tictactoe
+-make mancala
+-make snake
+-make connect_four
+-style games
+-reset button for games
+
+#tictactoe-todo
+-gamestate
+-gameloop
+-player picks a tile
+-mark it for player
+-check if winning
+-check it tie
+-players set names
+
+#mancala-todo
+-figure out mancala
+
+#Snake-todo
+-player score
+-player best
+-gameloop
+-snake moves without input
+-snake eats block
+-add point
+-set next block randomly
+-if snake would touch itself gameover
+-snake grows
+
+#connect_four-todo
+-players input names
+-gameloop
+-player picks a column to insert
+-chip goes to lowest possible level
+-if player has 4 chips in a row they win
--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -2,7 +2,8 @@
 let gamestate = {
   player: [{ name: "" }, { name: "" }],
 };
-
+let player1 = document.getElementById("player1");
+let player2 = document.getElementById("player2");
 //create gameboard
 let gameboard = document.getElementById("gameboard");
 for (let i = 0; i < 9; i++) {
@@ -11,20 +12,28 @@ for (let i = 0; i < 9; i++) {
   gameboard.append(boardel);
 }
 //player name submit
+//submit button
 let input = document.getElementById("submit");
+//adds name to gamestate
 function nameadd(input) {
+  //if gamesate player1 is taken
   if (gamestate.player[0].name == true) {
-    gamestate.player.name = input;
+    //add name to player 2
+    gamestate.player[1].name = input;
+    player2.innerText += gamestate.player[1].name;
+  } else {
+    //else add name to player 1
+    gamestate.player[0].name = input;
+    player1.innerText += gamestate.player[0].name;
   }
-  gamestate.player.name += input;
 }
-//list name
-
+//pick a tile
+gameboard.addEventListener("click", (e) => {});
 //reset board game
 //check if tile is taken
 //check if tie
 //check for win
-//
+//event listeners
 input.addEventListener("click", function () {
   nameadd(document.querySelector("input").value);
 });
